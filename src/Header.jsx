e51fdba1f105da575872dabe571e5dba728bfd02
Rohan-Shridhar <rohansm668@gmail.com>
2026-03-03 20:39:51 +0530

    Changed the Header font style to a pixel font

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,6 @@ const Header = () => {
     return (
         <div className="header-cont">
             <h1>GRIDCRAFT</h1>
-            <p>Tiny squares. Big imagination.</p>
         </div>
     );
 }
