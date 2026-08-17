275331e57b2ade69cf971ccf2c9218a234edccbd
Jiyavur <jiyavurrahman80@gmail.com>
2026-08-17 11:30:49 +0530

    feat: add 404 error page

---
+++ src/ErrorPage.jsx
@@ -0,0 +1,11 @@
+function ErrorPage() {
+  return (
+    <div>
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <p>The page you're looking for doesn't exist.</p>
+    </div>
+  );
+}
+
+export default ErrorPage;
